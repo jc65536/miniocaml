8a296342bd1660f942734eb4e60b4fcc5026d297
Jason Cheng <jasoncheng224@gmail.com>
2024-02-12 15:51:35 -0800

    Tweak grammar

--- test.ml
+++ test.ml
@@ -1,4 +1,4 @@
-let rec short_fold f init list = match list with
+let rec short_fold = fun f -> fun init -> fun list -> match list with
   | x :: xs -> let ( cont , acc ) = f init x in
     if cont then short_fold f acc xs else acc
   | _ -> init
