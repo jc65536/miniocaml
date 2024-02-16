600d9a8e0dd98d95f97cb45e4ce494f660d179a4
Jason Cheng <jasoncheng224@gmail.com>
2024-02-16 12:51:20 -0800

    Smaller example, and fix test1

--- tc.pl
+++ tc.pl
@@ -1,17 +1,6 @@
 parse_tree(
-let(true, var(all_eq),              
-  fun(var(la),
-    fun(var(lb),
-      match(tuple([var(la), var(lb)]), [
-        branch(tuple([list([]), list([])]), bool(true)),
-        branch(tuple([list([]), any]), bool(false)),
-        branch(tuple([any, list([])]), bool(false)),
-        branch(tuple([cons(var(x), var(xs)), cons(var(y), var(ys))]),
-          bop(bop(var(x), "=", var(y)), "&&",
-          app(var(all_eq), [var(ys), var(xs)])))]))),
-  tuple([
-    app(var(all_eq), [list([int(1), int(1)]), list([int(1), int(1)])]),
-    app(var(all_eq), [list([int(1)]), list([])])]))
+fun(cons(var(x), var(xs)),
+  fun(var(y), let(false, any, bop(var(x), "+", var(y)), var(xs))))
 ).
 
 % Symbol table
--- test1.ml
+++ test1.ml
@@ -1,8 +1,10 @@
-let rec short_fold = fun f -> fun init -> fun list -> match list with
-  | x :: xs -> let (cont, acc) = f init x in
-    if cont then short_fold f acc xs else acc
-  | _ -> init
+let not = fun b -> match b with | true -> false | false -> true
 in
-  let prod = short_fold (fun acc -> fun x -> (not (x = 0), acc * x)) 1
+  let rec short_fold = fun f -> fun init -> fun list -> match list with
+    | x :: xs -> let (cont, acc) = f init x in
+      if cont then short_fold f acc xs else acc
+    | _ -> init
   in
-    prod [3; 4; 5; 0; 6; 7]
+    let prod = short_fold (fun acc -> fun x -> (not (x = 0), acc * x)) 1
+    in
+      prod [3; 4; 5; 0; 6; 7]
